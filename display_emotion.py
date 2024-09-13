4eb16ccaa128c66d41857282a52bcaca159e3784
jeseoyun <jeeeseo98@gmail.com>
2024-09-13 15:29:19 +0900

    add: 프로젝트 생성

---
+++ display_emotion.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import Model, Sequential, load_model, model_from_json
+from tensorflow.compat.v1.keras.backend import set_session
+
+from facial_analysis import FacialImageProcessing
+
+
+def mobilenet_preprocess_input(x, **kwargs):
+    x[..., 0] -= 103.939
+    x[..., 1] -= 116.779
+    x[..., 2] -= 123.68
+    return x
+
+
+def main():
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.compat.v1.Session(config=config)
+    set_session(sess)
+
+    imgProcessing = FacialImageProcessing(False)
+
+    model = load_model('models/affectnet_emotions/mobilenet_7.h5')
+
+    preprocessing_function = mobilenet_preprocess_input
+    INPUT_SIZE = (224, 224)
+
+    idx_to_class = {
+        0: 'Anger',
+        1: 'Anger',     # Disgust
+        2: 'Anger',     # Fear
+        3: 'Happiness',
+        4: 'Neutral',
+        5: 'Sadness',
+        6: 'Surprise'
+    }
+
+    img_path = 'samples/5.jpg'
+    frame_bgr = cv2.imread(img_path)
+    frame = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+    bounding_boxes, points = imgProcessing.detect_faces(frame)
+    points = points.T
+
+    for bbox, p in zip(bounding_boxes, points):
+        box = bbox.astype(np.int32)
+        x1, y1, x2, y2 = box[0:4]
+        face_img = frame[y1:y2, x1:x2, :]
+
+        face_img = cv2.resize(face_img, INPUT_SIZE)
+        inp = face_img.astype(np.float32)
+        inp[..., 0] -= 103.939
+        inp[..., 1] -= 116.779
+        inp[..., 2] -= 123.68
+        inp = np.expand_dims(inp, axis=0)
+        scores = model.predict(inp)[0]
+        print(idx_to_class[np.argmax(scores)])
+
+
+if __name__ == "__main__":
+    main()
