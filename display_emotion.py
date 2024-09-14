a4eab35e8f798f150f784408d2e5f58f7bac88d8
Jeseoyun <jeeeseo98@gmail.com>
2024-09-14 12:43:38 +0900

    불필요한 코드 삭제 및 주석 추가

--- display_emotion.py
+++ display_emotion.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import cv2
 import tensorflow as tf
@@ -7,14 +8,7 @@ from tensorflow.compat.v1.keras.backend import set_session
 from facial_analysis import FacialImageProcessing
 
 
-def mobilenet_preprocess_input(x, **kwargs):
-    x[..., 0] -= 103.939
-    x[..., 1] -= 116.779
-    x[..., 2] -= 123.68
-    return x
-
-
-def main():
+def main(img_path):
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.compat.v1.Session(config=config)
@@ -24,26 +18,28 @@ def main():
 
     model = load_model('models/affectnet_emotions/mobilenet_7.h5')
 
-    preprocessing_function = mobilenet_preprocess_input
     INPUT_SIZE = (224, 224)
 
     idx_to_class = {
         0: 'Anger',
-        1: 'Anger',     # Disgust
-        2: 'Anger',     # Fear
+        1: 'Anger',     # 원본 label: Disgust
+        2: 'Anger',     # 원본 label: Fear
         3: 'Happiness',
         4: 'Neutral',
         5: 'Sadness',
         6: 'Surprise'
     }
 
-    img_path = 'samples/5.jpg'
     frame_bgr = cv2.imread(img_path)
     frame = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+
+    # 입력 이미지에서 얼굴 위치 찾기
+    # 여러 명의 얼굴도 인식 가능 : 프로젝트 요구사항에 따라 예외 처리 필
     bounding_boxes, points = imgProcessing.detect_faces(frame)
     points = points.T
 
     for bbox, p in zip(bounding_boxes, points):
+        # 입력 이미지 preprocessing
         box = bbox.astype(np.int32)
         x1, y1, x2, y2 = box[0:4]
         face_img = frame[y1:y2, x1:x2, :]
@@ -54,9 +50,13 @@ def main():
         inp[..., 1] -= 116.779
         inp[..., 2] -= 123.68
         inp = np.expand_dims(inp, axis=0)
+
+        # 이미지로부 표정 예측
         scores = model.predict(inp)[0]
         print(idx_to_class[np.argmax(scores)])
 
 
 if __name__ == "__main__":
-    main()
+    main(
+        img_path=sys.argv[1]
+    )
